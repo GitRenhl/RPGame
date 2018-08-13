01e6b261ce900689c5bb61b331c214296b64d6cb
GitRenhl <niemam10@gmail.com>
2018-08-13 15:37:18 +0200

    Add render function

--- util/Map.lua
+++ util/Map.lua
@@ -1,7 +1,6 @@
-Class = require "lib/class"
-Tile = require "util/Tile"
+local Tile = require "util/Tile"
 
-Map = Class {}
+local Map = Class {}
 
 function Map:init(width, height)
     self.WIDTH = width
@@ -15,5 +14,22 @@ function Map:load(source)
         for x = 1, self.WIDTH do
             self.tiles[y][x] = Tile.Grass.ID
         end
+    end
+    -- local i = 1
+    -- for y = 1, self.HEIGHT do
+    --     for x = 1, self.WIDTH do
+    --         self.tiles[i] = Tile.Grass.ID
+    --         i = i + 1
+    --     end
+    -- end
+end
+
+function Map:render(render_x, render_y, tile_x, tile_y, width, height)
+    for tile_y = tile_y, height do
+        for tile_x = tile_x, width do
+            Tile:render(self.tiles[tile_x][tile_y], render_x, render_y, TILE_SCALE)
+            render_x = render_x + TILE_SCALE
+        end
+        render_y = render_y + TILE_SCALE
     end
 end
