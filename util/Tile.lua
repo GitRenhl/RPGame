0f08e92b83aa0e497cec75a3e8dc51a27ba54f48
GitRenhl <niemam10@gmail.com>
2018-08-13 17:01:41 +0200

    creating module

--- util/Tile.lua
+++ util/Tile.lua
@@ -1,7 +1,5 @@
-Class = require "lib/class"
-
-Tile = Class {}
-Block = Class {}
+local Tile = Class {}
+local Block = Class {}
 
 local last_id = 1
 local function getID()
@@ -10,21 +8,51 @@ local function getID()
 end
 
 -- Block
-function Block:init(img_src)
-    self.img = img_src
-    self.color = {100, 255, 100}
+function Block:init(name, img_src, R, G, B, collision)
+    self.NAME = name
     self.ID = getID()
+    self.img = img_src
+    self.color = {R, G, B}
+    self.collision = false
+    if collision then
+        self.collision = collision
+    end
 end
 
-function Block:render(x, y, w, h)
-    love.graphics.rectangle("fill", x, y, w, h)
+function Block:render(x, y, scale)
+    love.graphics.setColor(self.color[1] / 255, self.color[2] / 255, self.color[3] / 255, 1)
+    love.graphics.rectangle("fill", x, y, scale, scale)
 end
 
 -- Tile
 function Tile:init()
-    self.Grass = Block("")
-    self.Water = Block("")
+    self.BLOCK_ID = {
+        GRASS = 1,
+        WATER = 2
+    }
+    self.blocks = {}
+    self.blocks[self.BLOCK_ID.GRASS] = Block("Grass", "", 100, 255, 100)
+    self.blocks[self.BLOCK_ID.WATER] = Block("Water", "", 100, 100, 255)
 end
 
-function Tile:render(block_name)
+function Tile:render(block_id, x, y, scale)
+    self.blocks[block_id]:render(x, y, scale)
 end
+
+-- AAA = {
+--     BLOCK_ID = {
+--         GRASS = 1,
+--         WATER = 2
+--     },
+--     blocks = {},
+--     render = function(block_id, x, y, scale)
+--         AAA.blocks[block_id]:render(x, y, scale)
+--     end
+-- }
+
+-- AAA.blocks[AAA.BLOCK_ID.GRASS] = Block("Grass", "")
+-- AAA.blocks[AAA.BLOCK_ID.WATER] = Block("Water", "")
+
+Tile = Tile()
+
+return Tile
