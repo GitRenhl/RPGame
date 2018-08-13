a8e7b4cfdade9a46d6ed722612000b294e194d47
GitRenhl <niemam10@gmail.com>
2018-08-13 16:59:47 +0200

    add render() and get_tile()

--- util/Map.lua
+++ util/Map.lua
@@ -12,7 +12,7 @@ function Map:load(source)
     for y = 1, self.HEIGHT do
         self.tiles[y] = {}
         for x = 1, self.WIDTH do
-            self.tiles[y][x] = Tile.Grass.ID
+            self.tiles[y][x] = math.random(1, 2)
         end
     end
     -- local i = 1
@@ -24,12 +24,32 @@ function Map:load(source)
     -- end
 end
 
+function Map:get_tile(x, y)
+    return self.tiles[y][x]
+end
+
 function Map:render(render_x, render_y, tile_x, tile_y, width, height)
-    for tile_y = tile_y, height do
-        for tile_x = tile_x, width do
-            Tile:render(self.tiles[tile_x][tile_y], render_x, render_y, TILE_SCALE)
-            render_x = render_x + TILE_SCALE
+    local r_x = render_x
+    for t_y = tile_y + 1, height do
+        for t_x = tile_x + 1, width do
+            Tile:render(self:get_tile(t_x, t_y), r_x, render_y, TILE_SCALE)
+            r_x = r_x + TILE_SCALE
         end
         render_y = render_y + TILE_SCALE
+        r_x = render_x
     end
 end
+
+function Map:render_auto(render_x, render_y, tile_x, tile_y)
+    local r_x = render_x
+    for t_y = tile_y + 1, VIRTUAL.HIGHT / TILE_SCALE do
+        for t_x = tile_x + 1, VIRTUAL.WIDTH / TILE_SCALE do
+            Tile:render(self:get_tile(t_x, t_y), r_x, render_y, TILE_SCALE)
+            r_x = r_x + TILE_SCALE
+        end
+        render_y = render_y + TILE_SCALE
+        r_x = render_x
+    end
+end
+
+return Map
